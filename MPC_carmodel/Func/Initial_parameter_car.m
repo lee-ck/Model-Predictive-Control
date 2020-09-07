3bb713b13a0ef2fe3d56cc5b0cc0d5f5a86477b8
Changyu Lee <leeck@kaist.ac.kr>
2020-09-08 01:49:55 +0900

    Update Initial_parameter_car.m

--- MPC_carmodel/Func/Initial_parameter_car.m
+++ MPC_carmodel/Func/Initial_parameter_car.m
@@ -1,17 +1,12 @@
 function [sim, input] = Initial_parameter_car(sim, input)
 %% Initial parameter setting
-% calculate Target_RPM which is calculated by target velocity
 
 sim.X0   = [sim.init_x   sim.init_y  ...   % x y 
             sim.init_psi ...   % psi
             sim.target_velocity    0    0 ];   %  velocity steer acceleration; 
-
-
 sim.Xref = sim.X0;
-
-Uref     = [0 0];           % DEL delta_sPort delta_sSB delta_r AddForeBallast RPM
-Uinit    = [0 0];           % DEL delta_sPort delta_sSB delta_r AddForeBallast RPM
-
+Uref     = [0 0];      
+Uinit    = [0 0];        
 
 input.x  = repmat(sim.X0,sim.Num+1,1);
 Xref     = repmat(sim.Xref,sim.Num,1);
@@ -23,8 +18,4 @@ input.u  = repmat(Uinit,sim.Num,1);
 
 input.y  = [Xref(1:sim.Num,:) Uref  zeros(sim.Num,1)];
 input.yN = Xref(sim.Num,:);
-
-
-
-
 end
