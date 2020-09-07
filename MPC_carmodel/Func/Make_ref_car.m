428a186949a21a7d89117664d9683eb7b147f58a
Changyu Lee <leeck@kaist.ac.kr>
2020-09-08 01:41:31 +0900

    Update Make_ref_car.m

--- MPC_carmodel/Func/Make_ref_car.m
+++ MPC_carmodel/Func/Make_ref_car.m
@@ -4,8 +4,6 @@ flag = Map.WPT - input.x0(1:2);
 [~, minidx]  = min(sum(flag.*flag,2));
 input.index = [];
 
-% Map.WPT(minidx,:)
-% minidx
 %% Stage reference (CTE)
 slack = minidx;
 for i = 1:sim.Num+1
@@ -32,19 +30,8 @@ for i = 1:sim.Num+1
     slack = slack +1;
 end
 
-% for i = 1:sim.Num
-%     Ax = input.y(i+1,1)-input.y(i,1);
-%     Ay = input.y(i+1,2)-input.y(i,2);
-%     input.y(i,3) = (atan2(Ay,Ax));
-% end
-% input.y(sim.Num+1,3) = input.y(sim.Num,3);
 
 %% Terminal reference (LOS)
-
-Ax = Map.WPT(Map.WPT_Now,1)-input.x0(1);
-Ay = Map.WPT(Map.WPT_Now,2)-input.x0(2);
-
-
 input.yN(1,1) = input.y(end,1);%Map.WPT(Map.WPT_Now,1);
 input.yN(1,2) = input.y(end,2);%Map.WPT(Map.WPT_Now,2);
 input.yN(1,3) = input.y(end,3); %atan2(Map.WPT(Map.WPT_Now,2)-input.x0(2),Map.WPT(Map.WPT_Now,1)-input.x0(1));
